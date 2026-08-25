de2f553d582cf037ba68578b4f784929ec0b40d2
2024a1r042 <2024a1r042@mietjammu.in>
2026-08-25 12:21:36 +0530

    Add conversion from kilometers to meters, cm, and mm

---
+++ KmtoM,CM,MM.py
@@ -0,0 +1,5 @@
+distance = float(input("Enter distance in km:"))
+meters = distance*1000
+cm = meters*100
+mm = cm * 10
+print(f"Distance in meters: ",meters," In centimeters: ",cm," In milimeters: ",mm)
